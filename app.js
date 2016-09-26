9c285cf51465278174897263a1367fdfa54b0e3b
Dillon Powers <dillon.c.powers@gmail.com>
2016-09-26 11:42:48 -0400

    database can sync

---
+++ app.js
@@ -0,0 +1,42 @@
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var path = require('path');
+var pg = require('pg');
+var hstore = require('pg-hstore'); 
+var nunjucks = require('nunjucks');
+
+//set up express
+var express = require('express');
+var app = express();
+
+//importing sequelize
+var models = require('./models/index');
+
+//nunjucks configuration
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+var env = nunjucks.configure('views', {noCache: true});
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+// var server = app.listen(3001, function() {
+// 	console.log('listening on port 3001')
+// })
+
+models.User.sync({})
+	.then(function() {
+		return models.Page.sync({})
+	})
+	.then(function() {
+		app.listen(3001, function() {
+			console.log('Server is listening on port 3001!');
+		});
+	})
+	.catch(console.error);
+
+
+app.get('/', function(req, res){
+	res.render('views/index.html')
+});
